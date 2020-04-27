8126ffde7aa91cdc12b2250dda47bad2746e332b
ROHITH JAYAN <rohithjayan@apples-MacBook-Pro-2.local>
2020-04-28 02:35:46 +0530

    first commit

---
+++ lib/eighthPage.dart
@@ -0,0 +1,294 @@
+import 'package:flutter/material.dart';
+import 'package:getflutter/getflutter.dart';
+import 'package:quizzup/fourthPage.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
+import 'package:quizzup/ninethPage.dart';
+class EighthPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            RaisedButton(
+              child: Icon(
+                Icons.arrow_back,
+                size: 29.0,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Column(
+
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Select Topics to Challenge',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
+
+
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/c.jpeg'),
+                          shape: GFAvatarShape.standard
+
+
+                      ),
+                      Text('Rohith'),
+                    ],
+                  ),
+
+                  Row(
+                    children:<Widget>[
+                      Column(
+                        children: <Widget>[
+                          GFAvatar(
+                              size: 55.0,
+                              backgroundImage:AssetImage('images/b.jpeg'),
+                              shape: GFAvatarShape.standard
+
+
+                          ),
+                          Text('English'),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/a.jpeg'),
+                          shape: GFAvatarShape.standard
+                      ),
+                      Text('Rahul'),
+                    ],
+                  ),
+
+                ],
+
+              ),
+
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/c.jpeg'),
+                          shape: GFAvatarShape.standard
+
+
+                      ),
+                      Text('Rohith'),
+                    ],
+                  ),
+
+                  Row(
+                    children:<Widget>[
+                      Column(
+                        children: <Widget>[
+                          GFAvatar(
+                              size: 55.0,
+                              backgroundImage:AssetImage('images/b.jpeg'),
+                              shape: GFAvatarShape.standard
+
+
+                          ),
+                          Text('English'),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/a.jpeg'),
+                          shape: GFAvatarShape.standard
+                      ),
+                      Text('Rahul'),
+                    ],
+                  ),
+
+                ],
+
+              ),
+
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/c.jpeg'),
+                          shape: GFAvatarShape.standard
+
+
+                      ),
+                      Text('Rohith'),
+                    ],
+                  ),
+
+                  Row(
+                    children:<Widget>[
+                      Column(
+                        children: <Widget>[
+                          GFAvatar(
+                              size: 55.0,
+                              backgroundImage:AssetImage('images/b.jpeg'),
+                              shape: GFAvatarShape.standard
+
+
+                          ),
+                          Text('English'),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/a.jpeg'),
+                          shape: GFAvatarShape.standard
+                      ),
+                      Text('Rahul'),
+                    ],
+                  ),
+
+                ],
+
+              ),
+
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/c.jpeg'),
+                          shape: GFAvatarShape.standard
+
+
+                      ),
+                      Text('Rohith'),
+                    ],
+                  ),
+
+                  Row(
+                    children:<Widget>[
+                      Column(
+                        children: <Widget>[
+                          GFAvatar(
+                              size: 55.0,
+                              backgroundImage:AssetImage('images/b.jpeg'),
+                              shape: GFAvatarShape.standard
+
+
+                          ),
+                          Text('English'),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      GFAvatar(
+                          size: 55.0,
+                          backgroundImage:AssetImage('images/a.jpeg'),
+                          shape: GFAvatarShape.standard
+                      ),
+                      Text('Rahul'),
+                    ],
+                  ),
+
+                ],
+
+              ),
+
+            ),
+
+          ],
+        ),
+      ),
+
+
+      bottomNavigationBar:
+      Padding(
+        padding: const EdgeInsets.all(35.0),
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(20),
+          ),
+
+          height: 50,
+          width: 100,
+          child: RaisedButton(
+              child: Text(
+                'Continue',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  color: Colors.white,
+                ),
+              ),
+              textColor: Colors.white,
+              color: Colors.green,
+              onPressed: () {
+                Navigator.push(context,
+                  new MaterialPageRoute(builder: (context) => new NinethPage()),
+                );
+              }
+          ),
+        ),
+      ),
+    );
+
+  }}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'firstPage.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home:  FirstScreen(),
+    );
+  }
+}
+
+
+
+
+
